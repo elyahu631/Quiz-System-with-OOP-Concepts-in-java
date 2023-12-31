08fb7e52f5639be952c7f4da5c5596e428e82f76
elyahu631 <eanavi363@gmail.com>
2023-12-31 08:40:44 +0200

    update comments

--- Quiz-System/src/Quiz.java
+++ Quiz-System/src/Quiz.java
@@ -1,3 +1,8 @@
 import java.util.List;
 
+/**
+ * A Quiz is a collection of questions.
+ * 
+ * @param questions a list of questions
+ */
 public record Quiz(List<IQuestion> questions) implements IQuiz {}
--- Quiz-System/src/QuizSystem.java
+++ Quiz-System/src/QuizSystem.java
@@ -3,10 +3,27 @@ import java.io.FileNotFoundException;
 import java.io.PrintWriter;
 import java.util.*;
 
+/**
+ * QuizSystem class is the main class of the Quiz System application.
+ * It contains the main method, which initializes the application and
+ * controls the flow of the application.
+ */
 public class QuizSystem {
+
     private static final Scanner scanner = new Scanner(System.in);
+
+    /**
+     * globalScores map stores the scores of all the quiz takers. The key is
+     * the name of the quiz taker, and the value is the score.
+     */
     private static final Map<String, Integer> globalScores = new HashMap<>();
 
+    /**
+     * main method is the entry point of the Quiz System application. It
+     * initializes the application and starts the main menu loop.
+     *
+     * @param args command line arguments
+     */
     public static void main(String[] args) {
         loadResults();
         while (true) {
@@ -32,6 +49,9 @@ public class QuizSystem {
         }
     }
 
+    /**
+     * Starts a new quiz and records the score of the quiz taker.     
+     */
     private static void startQuiz() {
         System.out.print("Enter your name: ");
         String name = scanner.next();
@@ -43,18 +63,30 @@ public class QuizSystem {
         globalScores.put(quizTaker.getName(), score);
     }
 
+    /**
+     * Generates a random quiz with the specified number of questions.
+     *
+     * @param numberOfQuestions number of questions in the quiz
+     * @return a list of questions
+     */
     private static List<IQuestion> generateRandomQuiz(int numberOfQuestions) {
         List<IQuestion> allQuestions = populateQuestions();
         Collections.shuffle(allQuestions);
         return allQuestions.subList(0, Math.min(numberOfQuestions, allQuestions.size()));
     }
 
+    /**
+     * Displays the scores of all the quiz takers.
+     */
     private static void displayResults() {
         for (Map.Entry<String, Integer> entry : globalScores.entrySet()) {
             System.out.println(entry.getKey() + ": " + entry.getValue());
         }
     }
 
+    /**
+     * sSaves the scores of all the quiz takers to a file.
+     */
     private static void saveResults() {
         try (PrintWriter out = new PrintWriter("quiz_results.txt")) {
             for (Map.Entry<String, Integer> entry : globalScores.entrySet()) {
@@ -65,6 +97,9 @@ public class QuizSystem {
         }
     }
 
+    /**
+     * Loads the scores of all the quiz takers from a file.
+     */
     private static void loadResults() {
         try (Scanner fileScanner = new Scanner(new File("quiz_results.txt"))) {
             while (fileScanner.hasNextLine()) {
@@ -76,6 +111,11 @@ public class QuizSystem {
         }
     }
 
+    /**
+     * Loads the questions from a file and returns a list of questions.
+     *
+     * @return a list of questions
+     */
     private static List<IQuestion> populateQuestions() {
         List<IQuestion> questions = new ArrayList<>();
         try (Scanner fileScanner = new Scanner(new File("questions.txt"))) {
